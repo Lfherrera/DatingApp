5111e8eb0d7a643d3b37604e41e26504251dab60
Luis Herrera <donluisherrera@example.com>
2020-12-15 10:41:22 -0700

    Added controller register, DTOs, added validation

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -7,15 +7,11 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
-{
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UsersController : ControllerBase
+{ 
+    public class UsersController : BaseAPIControllers
     {
         private readonly DataContext _context;
-
-    
-        public UsersController(DataContext context)
+           public UsersController(DataContext context)
         {
             _context = context;
 
